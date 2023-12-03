4999e350c857e3c27e9e457dc5be76c1f3a861d3
ituitis20-toy19 <toy.cemalettin.celal@gmail.com>
2023-12-03 13:57:22 +0300

    Add some crud functions
    repository, service and dto classes are added

---
+++ src/main/java/com/beed/repository/AppUserRepository.java
@@ -0,0 +1,17 @@
+package com.beed.repository;
+
+import com.beed.model.entity.AppUser;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    @Query("SELECT u.rate FROM AppUser u WHERE u.id = :id")
+    Optional<Double> getRateById(@Param("id") Long id);
+
+}
---
+++ src/main/java/com/beed/service/AppUserService.java
@@ -0,0 +1,44 @@
+package com.beed.service;
+
+import com.beed.model.dto.AppUserDto;
+import com.beed.model.entity.AppUser;
+import com.beed.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AppUserService {
+    @Autowired
+    AppUserRepository userRepository;
+
+    public static AppUserDto convertUserEntityToDto(AppUser user){
+        return AppUserDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .name(user.getName())
+                .surname(user.getSurname())
+                .rate(user.getRate())
+                .mail(user.getMail())
+                .phone(user.getPhone())
+                .build();
+    }
+
+    public static List<AppUserDto> mapUserEntityToDto (List<AppUser> userList){
+        return userList.stream().map(AppUserService::convertUserEntityToDto).toList();
+    }
+
+    public List<AppUserDto> getAllUsers(){
+        List<AppUser> users = userRepository.findAll();
+        return mapUserEntityToDto(users);
+    }
+
+    public AppUserDto getUserByID(Long id) {
+        AppUser user = userRepository.findById(id).get();
+        return convertUserEntityToDto(user);
+    }
+}
