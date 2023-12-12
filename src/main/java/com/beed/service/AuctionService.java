9799cfaa51b32e110b10f2b662c6aa0f28214325
Hilal <erdoganh19@itu.edu.tr>
2023-12-12 09:13:56 +0300

    Auction Util added

--- src/main/java/com/beed/service/AuctionService.java
+++ src/main/java/com/beed/service/AuctionService.java
@@ -1,6 +1,7 @@
 package com.beed.service;
 
 import com.beed.model.dto.AuctionDto;
+import static com.beed.utility.AuctionUtil.*;
 import com.beed.model.entity.Auction;
 import com.beed.repository.AuctionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +15,6 @@ public class AuctionService {
     @Autowired
     AuctionRepository auctionRepository;
 
-    public static AuctionDto convertAuctiontodto (Auction auction) {
-
-        return AuctionDto.builder()
-                .id(auction.getId())
-                .title(auction.getTitle())
-                .description(auction.getDescription())
-                .auctioneer(auction.getAuctioneer())
-                .startDate(auction.getStartDate())
-                .endDate(auction.getEndDate())
-                .minStartBid(auction.getMinStartBid())
-                .build();
-    }
 
     public AuctionDto getAuctionbyId(Long Id){
         Auction auction = auctionRepository.findById(Id).get();
