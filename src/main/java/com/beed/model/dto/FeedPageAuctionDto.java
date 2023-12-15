cf884ffa7e2c008289b708b197e473a07fcb4f6c
shintalha <sahintalha0@outlook.com>
2023-12-15 17:54:35 +0300

    Add getFeedPageAuction to repository and create its dto

---
+++ src/main/java/com/beed/model/dto/FeedPageAuctionDto.java
@@ -0,0 +1,15 @@
+package com.beed.model.dto;
+
+import lombok.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedPageAuctionDto implements Serializable {
+    private Long id;
+    private String title;
+    private double minStartBid;
+}
